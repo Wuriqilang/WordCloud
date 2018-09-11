1c69691dd4c7c8ee53b043a15285d45c928c120e
Wuriqilang <543677991@qq.com>
2018-09-11 23:13:48 +0800

    优化local路径

--- python.py
+++ python.py
@@ -2,8 +2,10 @@
 import matplotlib.pyplot as plt
 import jieba
 from wordcloud import WordCloud
+from os import path
+localpath=path.dirname(__file__)  #获取当前工作路径
 #获取文件，注意这里要看编码格式
-text = open(r'/Users/wuriqilang/Desktop/程序开发/project/Python/cd.txt','r').read()
+text = open(localpath+r'/cd.txt','r').read()
 #剪切单词
 text_cut=jieba.cut(text)
 #单词拼接
@@ -12,7 +14,7 @@ result=' '.join(text_cut)
 #生成次云图
 wc = WordCloud(
     #字体路径
-    font_path=r'/Users/wuriqilang/Desktop/程序开发/project/Python/simhei.ttf',
+    font_path=localpath+ r'/simhei.ttf',
     #背景颜色
     background_color='white',
     #图片宽度
