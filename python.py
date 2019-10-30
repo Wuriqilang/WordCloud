71c58e8323c44b2ece5807f8a6ed93c38b6f0b26
Wuriqilang <543677991@qq.com>
2019-10-30 18:36:53 +0800

    修改例子

--- python.py
+++ python.py
@@ -1,30 +1,31 @@
 ﻿# 词云图
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import jieba
 from wordcloud import WordCloud
 from os import path
-localpath=path.dirname(__file__)  #获取当前工作路径
-#获取文件，注意这里要看编码格式
-text = open(localpath+r'/cd.txt','r',encoding='UTF-8').read()
-#剪切单词
-text_cut=jieba.cut(text)
-#单词拼接
-result=' '.join(text_cut)
+localpath = path.dirname(__file__)  # 获取当前工作路径
+# 获取文件，注意这里要看编码格式
+text = open(localpath+r'/words.txt', 'r', encoding='UTF-8').read()
 
-#生成次云图
+# 剪切单词
+text_cut = jieba.cut(text)
+# 单词拼接
+result = ' '.join(text_cut)
+
+# 生成次云图
 wc = WordCloud(
-    #字体路径
-    font_path=localpath+ r'/simhei.ttf',
-    #背景颜色
+    # 字体路径
+    font_path=localpath + r'/simhei.ttf',
+    # 背景颜色
     background_color='white',
-    #图片宽度
+    # 图片宽度
     width=500,
     height=350,
-    #字体的大小
-    max_font_size=50,
-    min_font_size=10,
+    # 字体的大小
+    max_font_size=70,
+    min_font_size=5,
 )
-#生成词云图片
+# 生成词云图片
 wc.generate(result)
 plt.imshow(wc)
 plt.axis('off')
